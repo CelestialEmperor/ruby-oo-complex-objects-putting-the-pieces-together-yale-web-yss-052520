ba07c654d79f6812f4f8cd304cfbe55271ebd2e3
Jack Ma <mapeiyao@gmail.com>
2020-05-28 10:28:59 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,4 +3,19 @@ class Book
   def initialize(title)
   end
  
+  def title
+  end
+  
+  def author name
+  end
+ 
+  def author name
+  end
+  
+  def page count
+  end
+ 
+  def genre
+  end
+  
 end
